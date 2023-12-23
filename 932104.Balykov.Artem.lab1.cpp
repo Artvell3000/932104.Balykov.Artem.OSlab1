b441d8387c91b13783e540ecff5656e27c12640e
artem_balykov <artem.balikov@yandex.ru>
2023-12-23 12:40:13 +0700

    last

--- 932104.Balykov.Artem.lab1.cpp
+++ 932104.Balykov.Artem.lab1.cpp
@@ -10,7 +10,7 @@ binary_semaphore second_semaphore(0);
 
 string buffer;
 
-void thread_provider() {
+void thread_send() {
 	while (true) {
 		this_thread::sleep_for(chrono::seconds(1));
 		buffer = "sms";
@@ -20,7 +20,7 @@ void thread_provider() {
 	}
 }
 
-void thread_consumer() {
+void thread_recv() {
 	while (true) {
 		second_semaphore.acquire();
 		cout << "Событие обработано" << buffer << endl;
@@ -32,8 +32,9 @@ void thread_consumer() {
 int main() {
 	buffer = "";
 	setlocale(LC_ALL, "ru");
-	thread thread_first(thread_provider);
-	thread thread_second(thread_consumer);
-	thread_first.join();
-	thread_second.join();
+	thread thread1(thread_send);
+	thread thread2(thread_recv);
+	thread1.join();
+	thread2.join();
+	return 0;
 }
